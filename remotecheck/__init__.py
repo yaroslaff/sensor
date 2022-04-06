d78ea304e7ae2bf3eaa13a328ad738fb2f7e5fd3
Yaroslav Polyakov <yaroslaff@gmail.com>
2022-04-06 20:40:47 +0700

    version

--- remotecheck/__init__.py
+++ remotecheck/__init__.py
@@ -27,7 +27,7 @@ urllib3.disable_warnings()
 
 import async_dnsbl_client 
 
-version = '0.0.57'
+version = '0.0.58'
 
 if __name__ == '__main__':
     from remotecheck.forcedip import ForcedIPHTTPSAdapter
